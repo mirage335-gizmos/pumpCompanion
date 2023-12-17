1f922085361ded398cf6e016e37d51a3b79e73fa
mirage335 <spamfreemirage335@gmail.com>
2023-12-16 22:34:41 -0500

    Draft.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -36,7 +36,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='2591634041'
-export ub_setScriptChecksum_contents='1062003845'
+export ub_setScriptChecksum_contents='1819330227'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -44396,11 +44396,11 @@ _pumpCompanion-frame-kilo() {
 	# Eliminates dependency on radio protocol to adjust AGC , sync clock, etc. DC Balanced , large amplitude, large bandwidth.
 	#0w
 	#0011 0000 0111 0111
-	(while true ; do echo '3077' ; done) | xxd -p -r | head -c 2100 > "$HOME"/Downloads/_framed.rrf
+	(while true ; do echo '307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077' ; done) | xxd -p -r | head -c 2100 > "$HOME"/Downloads/_framed.rrf
 	
 	_pumpCompanion-frame-minimal "$@"
 	
-	(while true ; do echo '3077' ; done) | xxd -p -r | head -c 300 >> "$HOME"/Downloads/_framed.rrf
+	(while true ; do echo '307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077' ; done) | xxd -p -r | head -c 300 >> "$HOME"/Downloads/_framed.rrf
 	
 	_messagePlain_nominal 'done: _pumpCompanion-frame'
 	#_messageNormal '_pumpCompanion-frame: done'
@@ -44419,12 +44419,12 @@ _pumpCompanion-frame-mega() {
 	#0011 0000 0111 0111
 	# ( ((8*10^6)*log2(1+(100/1))) bits * 12 / 4 to megabytes
 	# 12 seconds at one quarter of 8MHz channel
-	(while true ; do echo '307730773077307730773077307730773077' ; done) | xxd -p -r | head -c 20000100 > "$HOME"/Downloads/_framed.rrf
+	(while true ; do echo '307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077' ; done) | xxd -p -r | head -c 20000100 > "$HOME"/Downloads/_framed.rrf
 	
 	
 	_pumpCompanion-frame-minimal "$@"
 	
-	(while true ; do echo '307730773077307730773077307730773077' ; done) | xxd -p -r | head -c 66100 >> "$HOME"/Downloads/_framed.rrf
+	(while true ; do echo '307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077307730773077' ; done) | xxd -p -r | head -c 66100 >> "$HOME"/Downloads/_framed.rrf
 	
 	_messagePlain_nominal 'done: _pumpCompanion-frame'
 	#_messageNormal '_pumpCompanion-frame: done'
@@ -44469,7 +44469,12 @@ _refresh_anchors() {
 	
 	cp -a "$scriptAbsoluteFolder"/_anchor.bat ./_pumpCompanion-frame.bat
 	cp -a "$scriptAbsoluteFolder"/_anchor.bat ./_pumpCompanion-deframe.bat
+	
+	
+	cp -a "$scriptAbsoluteFolder"/_anchor ./_pumpCompanion-frame-mega
+	cp -a "$scriptAbsoluteFolder"/_anchor ./_pumpCompanion-frame-mega.bat
 }
+
 
 #####Program
 
